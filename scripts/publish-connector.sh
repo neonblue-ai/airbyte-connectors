1c0af9a58f2fb8e7ccf644204532c28d7ca72302
Jasper Chan <jasperchan@gmail.com>
2024-10-09 17:19:55 -0700

    fix: update publish-connector script to use buildx and support multi-platform builds

--- scripts/publish-connector.sh
+++ scripts/publish-connector.sh
@@ -33,14 +33,15 @@ latest_tag="$image:latest"
 version_tag="$image:$connector_version"
 echo "Image version tag: $version_tag"
 
-docker manifest inspect $version_tag > /dev/null
+docker manifest inspect $version_tag >/dev/null
 if [ "$?" == 1 ]; then
-  docker build . \
+  docker buildx build . \
     --build-arg path=$connector_path \
     --build-arg version=$connector_version \
     --pull \
     -t $latest_tag \
     -t $version_tag \
+    --platform linux/amd64,linux/arm64 \
     --label "io.airbyte.version=$connector_version" \
     --label "io.airbyte.name=$image"
   docker push $latest_tag
